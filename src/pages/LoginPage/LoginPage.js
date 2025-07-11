1c8e13e4d59ab7319631439102d7898fd901211e
Steivin <steivin2000@gmail.com>
2025-07-11 11:49:32 -0500

    pagina del dashboard

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -59,7 +59,7 @@ function LoginPage() {
               </label>
             </div>
             <button type="submit" className="btn btn-primary w-100 mb-2">Entrar</button>
-            <button type="button" className="btn btn-primary w-100" onClick={handleGoogleLogin}>Iniciar con Google</button>
+            <button type="button" className="btn btn-danger w-100" onClick={handleGoogleLogin}>Iniciar con Google</button>
           </form>
         </div>
         <div className="card-footer text-center">
